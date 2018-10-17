43453672fc3d705858d81525e08ce1fcb6a465f5
mason <star.wang@live.com>
2018-10-17 15:14:39 +1300

    updated chart

--- about.md
+++ about.md
@@ -3,3 +3,7 @@ layout: page
 title: About
 permalink: /about/
 ---
+
+## About this website
+
+.....
---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+	$("#easy-tag").css("content","22");
+  $("#good-tag").css("content","0");
+  $("#fail-tag").css("content","0");
+});
