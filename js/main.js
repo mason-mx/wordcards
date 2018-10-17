b0da776773d6c6a3050d82cbb159c2db4f9193a9
Mason <m.wang@coherent-solutions.com>
2018-10-17 16:51:32 +1300

    added flash cards

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
 	$("#easy-tag").css("content","22");
   $("#good-tag").css("content","0");
   $("#fail-tag").css("content","0");
+	$(".flippable").click(function(){
+		$(this).toggleClass("flipme");
+	});
 });
